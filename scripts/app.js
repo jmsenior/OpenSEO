c6be5d9bd70ef797980d4585e3055a5f8ef42b7a
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2020-02-15 18:16:58 +0100

    testing

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,26 @@
 //scripts
 
 console.log('Hello');
+
+
+function DocumentLinks(){
+
+	var list = [];
+
+
+	var enlace = document.querySelectorAll('a');
+	//console.log(enlace);
+
+
+
+	//for( i=0; i<enlace.length; i++){
+		//list.push[i];
+	//}
+
+
+}
+
+
+
+
+DocumentLinks();
