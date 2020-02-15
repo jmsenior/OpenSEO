9c4470da4c88823afa92d0ed3c2db47b4cfe00b6
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2020-02-15 17:22:14 +0100

    Example

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 //scripts
 
 console.log('Hello');
+
+
+function DocumentLinks(){
+
+	var list = [];
+
+
+	var enlace = document.body.querySelectorAll('a');
+	console.log(enlace);
+	for(i=0, i<enlace.length(), i++){
+
+	}
+
+
+
+}
+DocumentLinks();
---
+++ scripts/app.js
@@ -0,0 +1,3 @@
+//scripts
+
+console.log('Hello');
