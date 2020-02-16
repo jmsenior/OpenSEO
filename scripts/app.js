5d22a2d6ba54871f2e09f249f13d905b1a96a60f
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2020-02-16 10:42:54 +0100

    Limpieza

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,13 @@
 //scripts
 
-console.log('Hello');
-
-
 function DocumentLinks(){
 
 	var list = '';
 
 
 	var enlace = document.querySelectorAll('a');
-	//console.log(enlace);
 
-	var myWindow = window.open('','','resizable=yes,scrollbars=yes,width=1000,height=800');
+	//var myWindow = window.open('','','resizable=yes,scrollbars=yes,width=1000,height=800');
 	
 	for( i=0; i<enlace.length; i++){
 		list += enlace[i].getAttribute('rel') + ' ' + enlace[i].href + '<br/>';
@@ -22,8 +18,5 @@ function DocumentLinks(){
 
 
 }
-
-
-
 
 DocumentLinks();
