68a6789a3c7aa43b9e7296403eb838405fcc93b2
José Manuel Serna Rodríguez <jm.sernarodriguez@gmail.com>
2020-02-16 11:14:05 +0100

    Start iframe content

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,26 @@
-//scripts
+// start content
 
-function DocumentLinks(){
+//function DocumentLinks(){
 
-	var list = '';
+//	var list = '';
 
+//	var enlace = document.querySelectorAll('a');
 
-	var enlace = document.querySelectorAll('a');
-
-	//var myWindow = window.open('','','resizable=yes,scrollbars=yes,width=1000,height=800');
+//	var myWindow = window.open('','','resizable=yes,scrollbars=yes,width=1000,height=800');
 	
-	for( i=0; i<enlace.length; i++){
-		list += enlace[i].getAttribute('rel') + ' ' + enlace[i].href + '<br/>';
-	}
+//	for( i=0; i<enlace.length; i++){
+//		list += enlace[i].getAttribute('rel') + ' ' + enlace[i].href + '<br/>';
+//	}
 
+//	myWindow.document.body.innerHTML = list;
 
-	myWindow.document.body.innerHTML = list;
+//}
 
+//DocumentLinks();
 
+function InsertIframe(){
+	var pagina = document.querySelector('body');
+
+	pagina.insertAdjacentHTML('afterend', '<iframe></iframe>');
 }
-
-DocumentLinks();
+InsertIframe();
